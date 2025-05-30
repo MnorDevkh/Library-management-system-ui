555d15cbb4ba91cae121f7cb13f476d55f2f40f4
Mnor <bourenmno1111@gmail.com>
2025-05-30 11:24:17 +0700

    ip

--- src/redux/service/api.js
+++ src/redux/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: "https://157.230.33.25:8443/api",
+    baseURL: "https://188.166.220.11:8443/api",
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
--- src/redux/service/apiAuth.js
+++ src/redux/service/apiAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiAuth = axios.create({
-    baseURL: "https://157.230.33.25:8443/api",
+    baseURL: "https://188.166.220.11:8443/api",
     headers: {
         "Accept": "*/*",
         "Content-Type": "application/json",
